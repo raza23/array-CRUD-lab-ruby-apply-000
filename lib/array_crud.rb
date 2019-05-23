20fd6570d33c3fcdbf4bc02b51750a46cf86f000
Raza Tahir Shareef <rtshareef@gmail.com>
2019-05-23 19:42:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -40,7 +40,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = [1,2,3,4]
+  array = [1,"am",3,4]
   index_number= [1]
   return index_number
   
